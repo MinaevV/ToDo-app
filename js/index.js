9feb92f451b917b09a52044820103ec19de394aa
Minaev Vlad <vminaev.exceedteam@gmail.com>
2021-06-24 19:46:46 +0300

    Init commit w/ add to localStorage function

---
+++ js/index.js
@@ -0,0 +1,90 @@
+const app = document.getElementById("app");
+const input = document.getElementById("app__input");
+const itemList = document.getElementById("app__item-list");
+const addBtn = document.getElementById("add");
+const delBtn = document.getElementById("del");
+
+localStorage.items;
+
+document.addEventListener("DOMContentLoaded", function () {
+  checkStorage();
+
+  function checkStorage() {
+    if (localStorage.items === undefined) {
+      console.log("localStorage пуст");
+    } else {
+      console.log("Не пуст");
+    }
+  }
+  function appendItem(el) {
+    // create item
+    const tempItem = document.createElement("div");
+    tempItem.className = "app__item";
+    tempItem.id = el[0];
+
+    // fill item with checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "label-" + el[0];
+    tempItem.append(checkbox);
+    // and lable
+    const label = document.createElement("label");
+    label.htmlFor = "label-" + el[0];
+    label.innerText = el[1];
+    tempItem.append(label);
+
+    // add delBtn
+    const delBtn = document.createElement("div");
+    delBtn.id = "del";
+    delBtn.className = "app__delete-item";
+    delBtn.innerText = "×";
+    delBtn.addEventListener("click", (e) => {
+      e.target.parentElement.remove();
+    });
+    tempItem.append(delBtn);
+
+    // append to list
+    if (document.contains(document.getElementById("app__item-list"))) {
+      itemList.append(tempItem);
+    }
+  }
+
+  app.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      validation();
+    }
+  });
+
+  addBtn.addEventListener("click", (e) => {
+    validation();
+  });
+
+  function validation() {
+    if (input.value.length) {
+      let val = input.value;
+      const stamp = Date.now();
+      let a = toArr(stamp, val);
+
+      function toArr(id, value) {
+        return [id, value];
+      }
+      let arr = [];
+      arr.push(a);
+
+      if (localStorage.items === undefined) {
+        localStorage.items = JSON.stringify(arr);
+      } else {
+        parsedItems = JSON.parse(localStorage.items);
+        parsedItems.push(arr);
+        localStorage.items = JSON.stringify(parsedItems);
+
+        console.log(a);
+      }
+
+      appendItem(a);
+      input.value = "";
+    }
+  }
+});
+
+// localStorage.items = 1;
