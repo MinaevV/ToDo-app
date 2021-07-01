d7c942f6b5499b9eded46445a52fa6711665c017
Minaev Vlad <vminaev.exceedteam@gmail.com>
2021-07-01 21:44:59 +0300

    refactoring

--- js/index.js
+++ js/index.js
@@ -1,184 +1,196 @@
-const app = document.getElementById("app");
-const input = document.getElementById("app__input");
-const itemList = document.getElementById("app__item-list");
-const addBtn = document.getElementById("add");
-const delBtn = document.getElementById("del");
-const clearBtn = document.getElementById("clear");
-let count = document.getElementById("app__item-count");
-let filterList = document.querySelectorAll(".app__filters > *");
+const app = document.getElementById('app');
+const input = document.getElementById('app__input');
+const itemList = document.getElementById('app__item-list');
+const addBtn = document.getElementById('add');
+const delBtn = document.getElementById('del');
+const clearBtn = document.getElementById('clear');
+let count = document.getElementById('app__item-count');
+let filterList = document.querySelectorAll('.app__filters > *');
 
-let popup = document.querySelector(".popup");
-let popupInner = document.querySelector(".popup-inner");
-let changeVal = document.getElementById("changeVal");
+let popup = document.querySelector('.popup');
+let popupInner = document.querySelector('.popup-inner');
+let changeVal = document.getElementById('changeVal');
 let tossed;
+let valueToEdit;
 
-const allFilter = document.getElementById("all");
-const completedFilter = document.getElementById("completed");
-const activeFilter = document.getElementById("active");
+const allFilter = document.getElementById('all');
+const completedFilter = document.getElementById('completed');
+const activeFilter = document.getElementById('active');
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (readLS("filter") !== undefined) {
-    filter(readLS("filter"));
-    const radio = document.getElementById(`${readLS("filter")}`);
-    radio.checked = true;
-  } else {
-    filter();
-  }
-
+document.addEventListener('DOMContentLoaded', function () {
   //
   // Functions
   //
 
-  function appendItem(el) {
-    // create item
-    const tempItem = document.createElement("div");
+  loadFromStorage();
 
-    tempItem.className = "app__item";
-    tempItem.id = el.id;
-
-    // fill item with checkbox
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = el.status;
-    checkbox.classList = "visually-hidden";
-
-    tempItem.append(checkbox);
-    // and lable
-    const check = document.createElement("div");
-    check.classList = "check";
-    check.addEventListener("click", (e) => {
-      if (e.target.classList == "check") {
-        e.target.parentElement.parentElement.children[0].checked =
-          !e.target.parentElement.parentElement.children[0].checked;
-
-        parsedItems = parse("items");
-        let i = parsedItems.findIndex(
-          (item) => item.id == e.target.parentElement.parentElement.id
-        );
-        parsedItems[i].status =
-          e.target.parentElement.parentElement.children[0].checked;
-        writeLS("items", JSON.stringify(parsedItems));
-      }
-    });
-
-    const text = document.createElement("span");
-    // call popup for item value change
-    text.addEventListener("dblclick", (e) => {
-      if (e.target.tagName == "SPAN") {
-        popup.classList.add("__active");
-        toss(e.target.parentElement.id);
-        changeVal.value = e.target.innerText;
-        changeVal.focus();
-      }
-    });
-    text.innerText = el.value;
-
-    text.append(check);
-    tempItem.append(text);
-
-    // add delBtn
-    const delBtn = document.createElement("div");
-    delBtn.id = "del";
-    delBtn.className = "app__delete-item";
-    delBtn.innerText = "×";
-    // delete item on 'cross' click
-    delBtn.addEventListener("click", (e) => {
-      parsedItems = parse("items");
-      const key = parsedItems.find(
-        (item) => item.id == e.target.parentElement.id
-      );
-      parsedItems.splice(parsedItems.indexOf(key), 1);
-
-      writeLS("items", JSON.stringify(parsedItems));
-      e.target.parentElement.remove();
-      checkHeight();
-    });
-
-    tempItem.append(delBtn);
-
-    // append to list
-    if (document.contains(document.getElementById("app__item-list"))) {
-      itemList.append(tempItem);
-      checkHeight();
-
-      counter();
+  function createCheckbox(state) {
+    const item = document.createElement('input');
+    item.type = 'checkbox';
+    if (state) {
+      item.checked = state;
     }
-    return (checkboxes = document.querySelectorAll("div.app__item input"));
+    return item;
   }
 
-  function checkStorage() {
-    if (readLS("items")) {
-      parsedItems = parse("items");
+  function createCheckmarker() {
+    const item = document.createElement('div');
+    item.classList.add('check');
+    return item;
+  }
+
+  function createDelBtn() {
+    const item = document.createElement('div');
+    item.id = 'del';
+    item.classList.add('app__delete-item');
+    item.innerText = '×';
+    return item;
+  }
+
+  function createSpan(value, id) {
+    const item = document.createElement('span');
+    item.id = `span-${id}`;
+    item.innerText = value;
+
+    return item;
+  }
+
+  function toggleCheckbox(e) {
+    if (e.target.classList == 'check') {
+      const id = e.target.parentElement.parentElement.id;
+      const parent = e.target.parentElement.parentElement;
+      parent.firstElementChild.checked ^= true;
+
+      parsedItems = parse('items');
+      let i = parsedItems.findIndex((item) => item.id == id);
+      parsedItems[i].status = parent.firstElementChild.checked;
+      writeLS('items', JSON.stringify(parsedItems));
+    }
+  }
+
+  function deleteItem(e) {
+    parsedItems = parse('items');
+    const i = parsedItems.findIndex(
+      (item) => item.id == e.target.parentElement.id
+    );
+    parsedItems.splice(i, 1);
+    e.target.parentElement.remove();
+    writeLS('items', JSON.stringify(parsedItems));
+  }
+
+  function createItem(id) {
+    const item = document.createElement('div');
+    item.classList.add('app__item');
+    item.id = id;
+
+    return item;
+  }
+
+  function createListNode(id, state, value) {
+    const item = createItem(id);
+    appendItem(createCheckbox(state), item);
+    const span = createSpan(value, id);
+    const checkbox = createCheckmarker();
+    appendItem(checkbox, span);
+    appendItem(span, item);
+    appendItem(createDelBtn(), item);
+
+    return item;
+  }
+
+  function addItemListeners(item) {
+    const checkbox = item.querySelector('.check');
+    const deleteBtn = item.querySelector('#del');
+    const span = item.querySelector('span');
+
+    checkbox.addEventListener('click', (e) => {
+      toggleCheckbox(e);
+      counter();
+      filter(readLS('filter'));
+    });
+
+    deleteBtn.addEventListener('click', (e) => {
+      deleteItem(e);
+      counter();
+    });
+
+    span.addEventListener('dblclick', (e) => {
+      openEditField(e);
+    });
+
+    return item;
+  }
+
+  function appendItem(appendWhat, appendTo) {
+    return appendTo.appendChild(appendWhat);
+  }
+
+  function toObj(id, value, status) {
+    return { id, value, status };
+  }
+
+  function loadFromStorage() {
+    if (readLS('items')) {
+      parsedItems = parse('items');
       parsedItems.forEach((el) => {
-        appendItem(el);
+        const item = createListNode(el.id, el.status, el.value);
+
+        appendItem(addItemListeners(item), itemList);
       });
+      filter(readLS('filter'));
       counter();
       checkHeight();
     }
   }
 
   function filter(passed) {
-    if (!passed) {
-      writeLS("filter", "all");
-      const radio = document.getElementById(`${localStorage.filter}`);
-      radio.checked = true;
-      itemList.setAttribute("filter", readLS("filter"));
-      allFilter.classList.add("__active");
-    } else {
-      itemList.innerHTML = "";
-      writeLS("filter", `${passed}`);
-      itemList.setAttribute("filter", passed);
+    let itemsInList = document.querySelectorAll('.app__item');
+    switch (passed) {
+      case 'all':
+        itemsInList.forEach((item) => (item.style.display = 'flex'));
+        allFilter.classList.add('__active');
+        return writeLS('filter', 'all');
 
-      if (readLS("items")) {
-        parsedItems = parse("items");
-      }
-
-      switch (passed) {
-        case "all":
-          checkStorage();
-          allFilter.classList.add("__active");
-          break;
-
-        case "completed":
-          if (readLS("items")) {
-            parsedItems.forEach((item) => {
-              if (item.status) {
-                appendItem(item);
-              }
-            });
+      case 'completed':
+        itemsInList.forEach((item) => (item.style.display = 'flex'));
+        itemsInList.forEach((item) => {
+          if (!item.firstChild.checked) {
+            item.style.display = 'none';
+            completedFilter.classList.add('__active');
           }
-          completedFilter.classList.add("__active");
-          break;
+        });
+        return writeLS('filter', 'completed');
 
-        case "active":
-          if (readLS("items")) {
-            parsedItems.forEach((item) => {
-              if (!item.status) {
-                appendItem(item);
-              }
-            });
+      case 'active':
+        itemsInList.forEach((item) => (item.style.display = 'flex'));
+        itemsInList.forEach((item) => {
+          if (item.firstChild.checked) {
+            item.style.display = 'none';
           }
-          activeFilter.classList.add("__active");
+        });
+        activeFilter.classList.add('__active');
+        return writeLS('filter', 'active');
 
-          break;
-      }
+      default:
+        allFilter.classList.add('__active');
+        return writeLS('filter', 'all');
     }
   }
 
-  function checkFilter(e) {
+  function applyFilter(e) {
     filterList.forEach((item) => {
-      item.classList.remove("__active");
+      item.classList.remove('__active');
       if (item === e.currentTarget) {
-        item.classList.add("__active");
+        item.classList.add('__active');
       }
     });
   }
 
   // show remaining active tasks
   function counter() {
-    if (readLS("items")) {
+    if (readLS('items')) {
       let i = 0;
-      parsedItems = parse("items");
+      parsedItems = parse('items');
       parsedItems.forEach((item) => {
         if (!item.status) {
           ++i;
@@ -186,7 +198,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
       count.innerHTML = `${i} left`;
     } else {
-      count.innerHTML = "0 left";
+      count.innerHTML = '0 left';
     }
   }
 
@@ -198,28 +210,19 @@ document.addEventListener("DOMContentLoaded", function () {
       const stamp = Date.now();
       const status = false;
       const tempObj = toObj(stamp, val, status);
+      const item = createListNode(stamp, status, val);
 
-      function toObj(id, value, status) {
-        return { id: id, value: value, status: status };
-      }
+      appendItem(addItemListeners(item), itemList);
 
-      if (!readLS("items")) {
-        writeLS("items", JSON.stringify([tempObj]));
-        appendItem(tempObj);
+      if (!readLS('items')) {
+        writeLS('items', JSON.stringify([tempObj]));
       } else {
-        parsedItems = parse("items");
+        parsedItems = parse('items');
         parsedItems.push(tempObj);
-        writeLS("items", JSON.stringify(parsedItems));
-        appendItem(tempObj);
-
-        filter(readLS("filter"));
+        writeLS('items', JSON.stringify(parsedItems));
       }
-
-      counter();
-
-      input.value = "";
-    } else {
-      input.value = "";
+      input.value = '';
+      filter(readLS('filter'));
     }
   }
 
@@ -245,9 +248,20 @@ document.addEventListener("DOMContentLoaded", function () {
   // scroll list if its height more than 500px
   function checkHeight() {
     if (itemList.scrollHeight > 500) {
-      itemList.classList.add("__scrolling");
+      itemList.classList.add('__scrolling');
     } else {
-      itemList.classList.remove("__scrolling");
+      itemList.classList.remove('__scrolling');
+    }
+  }
+
+  // call popup for item value change
+  function openEditField(e) {
+    if (e.target.tagName == 'SPAN') {
+      popup.classList.add('__active');
+      toss(e.target.parentElement.id);
+      changeVal.focus();
+      changeVal.value = e.target.innerText;
+      valueToEdit = e.target.id;
     }
   }
 
@@ -256,77 +270,83 @@ document.addEventListener("DOMContentLoaded", function () {
   //
 
   // add item on 'Enter' hit
-  app.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
+  app.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
       validation();
       checkHeight();
+      counter();
     }
   });
 
   // close popup on side click
-  popup.addEventListener("click", (e) => {
+  popup.addEventListener('click', (e) => {
     if (e.currentTarget === e.target) {
-      popup.classList.remove("__active");
+      popup.classList.remove('__active');
     }
   });
 
   // close popup on 'Esc' hit
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && popup.classList.contains("__active")) {
-      popup.classList.remove("__active");
-      changeVal.value = "";
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && popup.classList.contains('__active')) {
+      popup.classList.remove('__active');
+      changeVal.value = '';
     }
   });
 
   // save new value of item on 'Enter' hit
-  changeVal.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
+  changeVal.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
       const tempVal = changeVal.value.trim();
       if (tempVal.length) {
-        parsedItems = parse("items");
+        parsedItems = parse('items');
         let i = parsedItems.findIndex((item) => item.id == tossed);
         changeVal.value = parsedItems[i].value;
         parsedItems[i].value = tempVal;
 
-        writeLS("items", `${JSON.stringify(parsedItems)}`);
-        changeVal.value = "";
-        popup.classList.remove("__active");
-        itemList.innerHTML = "";
+        writeLS('items', `${JSON.stringify(parsedItems)}`);
+        changeVal.value = '';
+        popup.classList.remove('__active');
 
-        checkStorage();
+        let a = document.getElementById(`${valueToEdit}`);
+        a = tempVal;
       }
     }
   });
 
   // filter by ALL tasks
-  allFilter.addEventListener("click", (e) => {
-    filter("all");
-    checkFilter(e);
+  allFilter.addEventListener('click', (e) => {
+    filter('all');
+    applyFilter(e);
   });
 
   // filter by COMPLETED tasks
-  completedFilter.addEventListener("click", (e) => {
-    filter("completed");
-    checkFilter(e);
+  completedFilter.addEventListener('click', (e) => {
+    filter('completed');
+    applyFilter(e);
   });
 
   // filter by ACTIVE tasks
-  activeFilter.addEventListener("click", (e) => {
-    filter("active");
+  activeFilter.addEventListener('click', (e) => {
+    filter('active');
 
-    checkFilter(e);
+    applyFilter(e);
   });
 
   // clear completed tasks
-  clearBtn.addEventListener("click", () => {
-    itemList.innerHTML = "";
-
-    parsedItems = parse("items");
-
+  clearBtn.addEventListener('click', () => {
+    let itemsToDelete = document.querySelectorAll('.app__item');
+    parsedItems = parse('items');
     parsedItems = parsedItems.filter((item) => !item.status);
-    writeLS("items", JSON.stringify(parsedItems));
-    checkStorage();
+    writeLS('items', JSON.stringify(parsedItems));
+
+    parsedItems = itemsToDelete.forEach((item) => {
+      if (item.firstElementChild.checked) {
+        item.remove();
+      }
+    });
     checkHeight();
-    filter(readLS("filter"));
+    counter();
+
+    // filter(readLS('filter'));
   });
 });
